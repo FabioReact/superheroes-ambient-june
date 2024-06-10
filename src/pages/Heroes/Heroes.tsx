8141c0b641e1f689a0f4cb5f1aa56646522eb46f
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2024-06-10 15:02:51 +0200

    feat: add loading indicator on heroes page

--- src/pages/Heroes/Heroes.tsx
+++ src/pages/Heroes/Heroes.tsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from 'react';
 import { Hero } from '../../types/hero';
 import { getHeroes } from '../../api/heroes';
+import { Loader } from '../../components/Loader/Loader';
 
 const Heroes = () => {
   const [heroes, setHeroes] = useState<Hero[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    getHeroes().then((data) => {
-      setHeroes(data);
-    });
+    getHeroes()
+      .then((data) => {
+        setHeroes(data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
   return (
     <section>
       <h1>Heroes</h1>
+      {isLoading && <Loader />}
       <ul>
         {heroes.map((hero) => (
           <li key={hero.id}>
