7546f5b184cab81c4422f503648d0149df938259
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2024-06-10 15:17:37 +0200

    feat: add HeroCard to heroes page

--- src/pages/Heroes/Heroes.tsx
+++ src/pages/Heroes/Heroes.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Hero } from '../../types/hero';
 import { getHeroes } from '../../api/heroes';
 import { Loader } from '../../components/Loader/Loader';
+import HeroCard from '../../components/HeroCard/HeroCard';
 
 const Heroes = () => {
   const [heroes, setHeroes] = useState<Hero[]>([]);
@@ -17,12 +18,12 @@ const Heroes = () => {
   }, []);
   return (
     <section>
-      <h1>Heroes</h1>
+      <h1 className='text-center'>Heroes</h1>
       {isLoading && <Loader />}
-      <ul>
+      <ul className='flex flex-wrap justify-center gap-4'>
         {heroes.map((hero) => (
           <li key={hero.id}>
-            {hero.id} - {hero.name}
+            <HeroCard hero={hero} />
           </li>
         ))}
       </ul>
