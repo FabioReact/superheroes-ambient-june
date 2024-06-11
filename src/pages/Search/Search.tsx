83360b01ba5d481aa544d16bb79accafddfe3ffd
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2024-06-11 11:06:40 +0200

    feat: add loading and error state

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -2,19 +2,31 @@ import { useRef, useState } from 'react';
 import { getHeroesByName } from '../../api/heroes';
 import { Hero } from '../../types/hero';
 import HeroCard from '../../components/HeroCard/HeroCard';
+import { Loader } from '../../components/Loader/Loader';
 
 // http://localhost:4000/heroes?name_like={hero}
 
 const Search = () => {
   const heroNameRef = useRef<HTMLInputElement>(null);
   const [heroes, setHeroes] = useState<Hero[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const onSubmitHandler = async (event: React.FormEvent) => {
     event.preventDefault();
     // Optionnal Chaining
     const heroName = heroNameRef.current?.value;
-    const data = await getHeroesByName(heroName!);
-    setHeroes(data);
+    setLoading(true);
+    try {
+      setIsError(false);
+      setHeroes([]);
+      const data = await getHeroesByName(heroName!);
+      setHeroes(data);
+    } catch (e) {
+      setIsError(true);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -26,10 +38,9 @@ const Search = () => {
           <input ref={heroNameRef} type='text' id='hero-name' name='hero-name' />
         </fieldset>
         <button>Search</button>
+        {isError && <p className='text-red-500'>An error happened while fetching</p>}
         <div className='flex justify-center flex-wrap gap-4'>
-          {heroes.map((hero) => (
-            <HeroCard key={hero.id} hero={hero} />
-          ))}
+          {loading ? <Loader /> : heroes.map((hero) => <HeroCard key={hero.id} hero={hero} />)}
         </div>
       </form>
     </section>
