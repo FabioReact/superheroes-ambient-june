f082d8696b3a1bffd4236cc241aab32ecf5236e9
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2024-06-11 11:27:08 +0200

    feat: add subscription to email and passwords inputs

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,20 +1,57 @@
+import { useState } from 'react';
+
 const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+
+  const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const onChangePasswordConfirmation = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordConfirmation(event.target.value);
+  };
+
+  const onSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
   return (
     <section>
       <h1>Register</h1>
-      <form>
+      <form onSubmit={onSubmitHandler}>
         <fieldset>
           <label htmlFor='email'>Email:</label>
-          <input type='email' id='email' name='email' />
+          <input value={email} onChange={onChangeEmail} type='email' id='email' name='email' />
         </fieldset>
         <fieldset>
           <label htmlFor='password'>Password:</label>
-          <input type='text' id='password' name='password' />
+          <input
+            value={password}
+            onChange={onChangePassword}
+            type='text'
+            id='password'
+            name='password'
+          />
         </fieldset>
         <fieldset>
           <label htmlFor='passwordConfirmation'>Confirm Password:</label>
-          <input type='text' id='passwordConfirmation' name='passwordConfirmation' />
+          <input
+            value={passwordConfirmation}
+            onChange={onChangePasswordConfirmation}
+            type='text'
+            id='passwordConfirmation'
+            name='passwordConfirmation'
+          />
         </fieldset>
+        {passwordConfirmation !== '' && password !== passwordConfirmation && (
+          <p className='text-xs text-red-500'>Password must match each other</p>
+        )}
         <button>Register</button>
       </form>
     </section>
