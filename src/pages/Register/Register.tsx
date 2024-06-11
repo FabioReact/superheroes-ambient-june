c8ca82da1bbdb2a438cdebb6a712c5c7d45008a7
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2024-06-11 12:25:44 +0200

    chore: add absolute paths

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,57 +1,60 @@
-import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+// type Inputs = {
+//   email: string;
+//   password: string;
+//   passwordConfirmation: string;
+// };
+
+const schema = z
+  .object({
+    email: z.string().email('Not a valid').min(8, 'Email shoud be at least 8 characters long'),
+    password: z.string().min(6, 'Password shoud be at least 6 characters long'),
+    passwordConfirmation: z.string(),
+  })
+  .refine((fields) => fields.password === fields.passwordConfirmation, {
+    message: 'Password should match',
+    path: ['passwordConfirmation'],
+  });
+
+type Inputs = z.infer<typeof schema>;
 
 const Register = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>({ resolver: zodResolver(schema) });
 
-  const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  };
-
-  const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
-
-  const onChangePasswordConfirmation = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPasswordConfirmation(event.target.value);
-  };
-
-  const onSubmitHandler = (event: React.FormEvent) => {
-    event.preventDefault();
+  const onSubmitHandler: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
   };
 
   return (
     <section>
       <h1>Register</h1>
-      <form onSubmit={onSubmitHandler}>
+      <form onSubmit={handleSubmit(onSubmitHandler)}>
         <fieldset>
           <label htmlFor='email'>Email:</label>
-          <input value={email} onChange={onChangeEmail} type='email' id='email' name='email' />
+          <input type='email' id='email' {...register('email')} />
+          {errors.email && <p className='text-xs text-red-500'>Message: {errors.email.message}</p>}
         </fieldset>
         <fieldset>
           <label htmlFor='password'>Password:</label>
-          <input
-            value={password}
-            onChange={onChangePassword}
-            type='text'
-            id='password'
-            name='password'
-          />
+          <input type='text' id='password' {...register('password')} />
+          {errors.password && (
+            <p className='text-xs text-red-500'>Message: {errors.password.message}</p>
+          )}
         </fieldset>
         <fieldset>
           <label htmlFor='passwordConfirmation'>Confirm Password:</label>
-          <input
-            value={passwordConfirmation}
-            onChange={onChangePasswordConfirmation}
-            type='text'
-            id='passwordConfirmation'
-            name='passwordConfirmation'
-          />
+          <input type='text' id='passwordConfirmation' {...register('passwordConfirmation')} />
+          {errors.passwordConfirmation && (
+            <p className='text-xs text-red-500'>Message: {errors.passwordConfirmation.message}</p>
+          )}
         </fieldset>
-        {passwordConfirmation !== '' && password !== passwordConfirmation && (
-          <p className='text-xs text-red-500'>Password must match each other</p>
-        )}
         <button>Register</button>
       </form>
     </section>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@api/*': './src/api/*',
+      '@components/*': './src/components/*',
+      '@pages/*': './src/pages/*',
+      'types/*': './src/types/*',
+    },
+  },
   plugins: [react()],
-})
+});
