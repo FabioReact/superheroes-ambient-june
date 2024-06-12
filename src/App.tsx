cb6f431f2642fec96438acc2293f798794eebd27
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2024-06-12 12:22:16 +0200

    feat: add react query to HeroById

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { router } from './router';
 import AuthContextProvider from './hoc/AuthContextProvider/AuthContextProvider';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <AuthContextProvider>
-      <RouterProvider router={router} />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/HeroById/HeroById.tsx
+++ src/pages/HeroById/HeroById.tsx
@@ -1,18 +1,27 @@
 import HeroCard from '@components/HeroCard/HeroCard';
 import { Loader } from '@components/Loader/Loader';
 import { useParams } from 'react-router-dom';
-import { useGetHeroById } from '../../hooks/useGetHeroById';
+import { useQuery } from '@tanstack/react-query';
+import { getHeroById } from '@api/heroes';
 
 const HeroById = () => {
   const { id } = useParams();
-  const { hero, isLoading, isError, error } = useGetHeroById(id!);
+  const {
+    data: hero,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ['getHeroById', id],
+    queryFn: () => getHeroById(id!),
+  });
 
   return (
     <section>
       <h1>Hero Name with id: {id}</h1>
       {isLoading && <Loader />}
       <div className='flex justify-center'>
-        {isError && <p className='text-sm text-red-600'>Error: {error}</p>}
+        {isError && <p className='text-sm text-red-600'>Error: {error.message}</p>}
         {hero && !isError && <HeroCard hero={hero} />}
       </div>
     </section>
