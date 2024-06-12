20f9c5aa155a40780cf09ec96d9195ca3a6a2f1b
Fabio Ginja Domingues <fabio.domingues@codeworks.fr>
2024-06-12 10:19:27 +0200

    refactor: useForm in Search page instead of useRef

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,55 +1,38 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { getHeroesByFilters } from '../../api/heroes';
 import { Hero } from '../../types/hero';
 import HeroCard from '../../components/HeroCard/HeroCard';
 import { Loader } from '../../components/Loader/Loader';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type Inputs = {
+  heroName: string;
+  intelligence: string;
+  combat: string;
+  durability: string;
+  power: string;
+  speed: string;
+  strength: string;
+};
 
 const Search = () => {
-  const heroNameRef = useRef<HTMLInputElement>(null);
-  const intelligenceRef = useRef<HTMLInputElement>(null);
-  const combatRef = useRef<HTMLInputElement>(null);
-  const durabilityRef = useRef<HTMLInputElement>(null);
-  const powerRef = useRef<HTMLInputElement>(null);
-  const speedRef = useRef<HTMLInputElement>(null);
-  const strengthRef = useRef<HTMLInputElement>(null);
-  const stats = [
-    { name: 'intelligence', ref: intelligenceRef },
-    { name: 'combat', ref: combatRef },
-    { name: 'durability', ref: durabilityRef },
-    { name: 'power', ref: powerRef },
-    { name: 'speed', ref: speedRef },
-    { name: 'strength', ref: strengthRef },
-  ];
+  const { register, handleSubmit } = useForm<Inputs>();
+  const stats = ['intelligence', 'combat', 'durability', 'power', 'speed', 'strength'];
 
   const [heroes, setHeroes] = useState<Hero[]>([]);
   const [loading, setLoading] = useState(false);
   const [isError, setIsError] = useState(false);
 
-  const onSubmitHandler = async (event: React.FormEvent) => {
-    event.preventDefault();
-    // Optionnal Chaining
-    const heroName = heroNameRef.current?.value || '';
-    const intelligence = intelligenceRef.current?.value || '0';
-    const combat = combatRef.current?.value || '0';
-    const durability = durabilityRef.current?.value || '0';
-    const power = powerRef.current?.value || '0';
-    const speed = speedRef.current?.value || '0';
-    const strength = strengthRef.current?.value || '0';
+  const onSubmitHandler: SubmitHandler<Inputs> = async ({ heroName: name, ...rest }) => {
     setLoading(true);
     try {
       setIsError(false);
       setHeroes([]);
-      // const data = await getHeroesByName(heroName!);
-      const data = await getHeroesByFilters({
-        name: heroName,
-        intelligence,
-        speed,
-        combat,
-        durability,
-        power,
-        strength,
+      const heroes = await getHeroesByFilters({
+        name,
+        ...rest,
       });
-      setHeroes(data);
+      setHeroes(heroes);
     } catch (e) {
       setIsError(true);
     } finally {
@@ -60,15 +43,16 @@ const Search = () => {
   return (
     <section>
       <h1>Search Hero</h1>
-      <form onSubmit={onSubmitHandler}>
+      <form onSubmit={handleSubmit(onSubmitHandler)}>
         <fieldset>
           <label htmlFor='hero-name'>Hero:</label>
-          <input ref={heroNameRef} type='text' id='hero-name' name='hero-name' />
+          <input type='text' id='hero-name' {...register('heroName')} />
         </fieldset>
         {stats.map((stat) => (
           <fieldset>
-            <label htmlFor={stat.name}>{stat.name}:</label>
-            <input ref={stat.ref} type='range' id={stat.name} name={stat.name} defaultValue='0' />
+            <label htmlFor={stat}>{stat}:</label>
+            {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+            <input type='range' id={stat} defaultValue='0' {...register(stat as any)} />
           </fieldset>
         ))}
         <button>Search</button>
